19076281fcbe34eae29f9576148ebd0513eeb1ed
sergio <elmatadorsergio1234@gmail.com>
2024-06-23 11:12:51 -0400

    cambios de login y diseño

--- administrador/urls.py
+++ administrador/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('alquileres/', views.alquileres, name='alquileres'),
+    #logueamiento
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rentamotos/views.py
+++ rentamotos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Alquiler
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'home.html')
@@ -10,3 +11,22 @@ def alquileres(request):
         'alquileres': alquileres_list,
     }
     return render(request, 'alquileres.html', context)
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Usuario y/o contraseña incorrectos."
+            return render(request, 'signin.html', {'error_message': error_message})
+    return render(request, 'signin.html')
